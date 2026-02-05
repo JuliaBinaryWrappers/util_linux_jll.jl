3b51dec8533890c7016d64c17d4309154a017246
jlbuild <juliabuildbot@gmail.com>
2026-02-05 20:04:19 +0000

    util_linux_jll build 2.41.3+0

--- src/util_linux_jll.jl
+++ src/util_linux_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("util_linux")
-JLLWrappers.@generate_main_file("util_linux", UUID("a762b42e-dc87-5958-a639-9c9eec9c0153"))
+JLLWrappers.@generate_main_file("util_linux", Base.UUID("a762b42e-dc87-5958-a639-9c9eec9c0153"))
 end  # module util_linux_jll
